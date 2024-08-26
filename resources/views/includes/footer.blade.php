1c7c60682ecc2b8904fad1a7568996e06d953114
AjRoBSeYeR <ajrob.seyer@gmail.com>
2024-08-26 12:58:44 -0600

    footer links

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,14 +3,20 @@
           Centro de enseñanza y capacitación © <?php echo date("Y"); ?><br>
     </div>
     <div class="social">
-        <a href="#">
-            <img src="{{ asset('assets/img/brands/facebook.svg') }}" alt="Facebook" style="width: 20px; height: 20px;">
+        <a href="https://www.facebook.com/profile.php?id=61559372526633">
+            <img src="{{ asset('assets/img/brands/facebook.svg') }}" alt="Facebook" style="width: 25px; height: 25px;">
         </a>&nbsp; | &nbsp;
-        <a href="#">
-            <img src="{{ asset('assets/img/brands/x.svg') }}" alt="X" style="width: 20px; height: 20px;">
+        <a href="https://www.instagram.com/dragonescec">
+            <img src="{{ asset('assets/img/brands/instagram.svg') }}" alt="Instagram" style="width: 25px; height: 25px;">
         </a>&nbsp; | &nbsp;
-        <a href="#">
-            <img src="{{ asset('assets/img/brands/tiktok.svg') }}" alt="Tik Tok" style="width: 20px; height: 20px;">
+        <a href="https://www.tiktok.com/@dragonescec">
+            <img src="{{ asset('assets/img/brands/tiktok.svg') }}" alt="Tik Tok" style="width: 25px; height: 25px;">
+        </a>&nbsp; | &nbsp;
+        <a href="https://www.threads.net/@dragonescec">
+            <img src="{{ asset('assets/img/brands/threads.svg') }}" alt="Threads" style="width: 25px; height: 25px;">
+        </a>&nbsp; | &nbsp;
+        <a href="https://www.youtube.com/@dragonescec">
+            <img src="{{ asset('assets/img/brands/youtube.svg') }}" alt="Youtube" style="width: 25px; height: 25px;">
         </a>
     </div>
 </footer>
