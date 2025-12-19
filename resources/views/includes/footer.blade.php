3f4c007debd37d01f6b7fd6b874cecc8a1501034
AjRoBSeYeR <ajrob.seyer@gmail.com>
2025-12-19 10:23:57 -0600

    added cci config

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
           Centro de enseñanza y capacitación © <?php echo date("Y"); ?><br>
     </div>
     <div class="social">
-        <a href="https://www.facebook.com/profile.php?id=61559372526633">
+        <a href="https://www.facebook.com/dragonescec/">
             <img src="{{ asset('assets/img/brands/facebook.svg') }}" alt="Facebook" style="width: 25px; height: 25px;">
         </a>&nbsp; | &nbsp;
         <a href="https://www.instagram.com/dragonescec">
@@ -15,7 +15,7 @@
         <a href="https://www.threads.net/@dragonescec">
             <img src="{{ asset('assets/img/brands/threads.svg') }}" alt="Threads" style="width: 25px; height: 25px;">
         </a>&nbsp; | &nbsp;
-        <a href="https://www.youtube.com/@dragonescec">
+        <a href="https://www.youtube.com/@DragonesOnAir">
             <img src="{{ asset('assets/img/brands/youtube.svg') }}" alt="Youtube" style="width: 25px; height: 25px;">
         </a>
     </div>
